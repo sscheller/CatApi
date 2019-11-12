e9ec43637ddb86de1c8ae3d8eeb57962ec42975f
Sonja Scheller <sscheller98@live.com>
2019-11-11 18:48:19 -0800

    Styling
    *Added A new title
    *Centered title
    *Changed Typeface
    *Changed button color
    *changed background color
    *added photo shadow

--- script.js
+++ script.js
@@ -14,16 +14,9 @@ $.getJSON( getStuff, setStuff, doStuff);
 
 var showStuff = function(data) {console.log(data[0].url);
                                 
-                                $('img').attr( 'src' , data[0].url  );
-                                
-       }
+      $('img').attr( 'src' , data[0].url  );                             };
 
-
-
-       function changeImage() {
-        var src = "https://api.thecatapi.com/v1/images/search";
-        $.getJSON(src, function(result){
-            $('body').css('img', 'url('+randomPhoto+')');
+        
+      
+      
     
-        });
-    }; 
